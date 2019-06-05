bc8bd8e362457d384f0d0026e458e0d1c471d1fd
Joe Lee <jdlee6@buffalo.edu>
2019-06-04 22:44:14 -0400

    pictures & started pagination

--- website/app/models.py
+++ website/app/models.py
@@ -8,6 +8,7 @@ class Result(db.Model):
     rating = db.Column(db.String(5), unique=False, nullable=False)
     distance = db.Column(db.String(10), unique=False, nullable=False)
     duration = db.Column(db.String(10), unique=False, nullable=False)
+    photo = db.Column(db.String(60), unique=False, nullable=False)
 
     def __repr__(self):
         return f"Result('{self.origin}, {self.name}', '{self.address}', '{self.rating}', '{self.distance}', '{self.duration}')"
--- website/app/routes.py
+++ website/app/routes.py
@@ -1,5 +1,5 @@
-from flask import redirect, url_for, render_template
-import googlemaps, json, os
+from flask import redirect, url_for, render_template, request
+import googlemaps, json, os, requests
 from app.forms import SearchForm
 from geopy.geocoders import Nominatim
 from app import app, API_KEY, db
@@ -8,6 +8,13 @@ from app.models import Result
 
 gmaps = googlemaps.Client(key=API_KEY)
 query = ['skatepark', 'skate park']
+default_url = 'https://maps.googleapis.com/maps/api/place/photo?'
+width = '300'
+
+
+def db_reset():
+    db.drop_all()
+    db.create_all()
 
 
 @app.route('/')
@@ -21,7 +28,7 @@ def home():
 def results():
     places_results = []
     form = SearchForm()
-    db.create_all()
+
     geolocator = Nominatim(user_agent="myapplication")
     city = form.location.data
     location = geolocator.geocode(city)
@@ -31,18 +38,40 @@ def results():
 
     for park in skatepark_result['results']:
         address = park['formatted_address']
-        distance_response = gmaps.distance_matrix(origins=city, destinations=address, mode='driving')
+        distance_response = gmaps.distance_matrix(origins=f'{latitude}, {longitude}', destinations=address, transit_mode='driving')
         distance = distance_response['rows'][0]['elements'][0]['distance']['text']
         duration = distance_response['rows'][0]['elements'][0]['duration']['text']
+
+        # retrieve photo url (some don't have photos)
+        try:
+            for photo in park['photos']:
+                reference = photo['photo_reference']
+                photo_url = requests.get(default_url + 'maxwidth=' + width +'&photoreference=' + reference + '&key=' + API_KEY).url
+        except Exception as e:
+            print(f'{e}')
+
         entry = Result(origin=city,
                         name=park['name'],
                         address=park['formatted_address'],
                         rating=park['rating'],
                         distance=distance,
-                        duration=duration)
+                        duration=duration,
+                        photo=photo_url)
         places_results.append(entry)
-        print(city, park['name'], address, park['rating'], distance, duration)
 
+        print(city, park['name'], address, park['rating'], distance, duration, photo_url)
+
+    db_reset()
     db.session.add_all(places_results)
     db.session.commit()
-    return render_template('results.html', form=form, results=places_results)
+
+    # pagination
+    page = request.args.get('page', 1, type=int)
+    page_results = Result.query.paginate(page=page, per_page=5)
+
+    return render_template('results.html', form=form, results=page_results)
+
+''' Problem: When you click on the a page button it conducts
+a brand new search but this time with no origin data which 
+ultimately fails to load the next page . . . 
+Works perfectly fine if I don't paginate it '''
