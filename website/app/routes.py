fa0cf8a98467cb927f3e27046b588e980c3f831f
Joe Lee <jdlee6@buffalo.edu>
2019-06-05 19:17:13 -0400

    completed sort-by menu

--- website/app/routes.py
+++ website/app/routes.py
@@ -70,3 +70,38 @@ def results():
     page_results = Result.query.order_by(Result.duration.asc()).paginate(page=page, per_page=5)
 
     return render_template('results.html', form=form, results=page_results, origin=city)
+
+# sort by routes
+@app.route('/rate_high', methods=['GET', 'POST'])
+def rate_high():
+    city = Result.query.with_entities(Result.origin).limit(1).scalar()
+    page = request.args.get('page', 1, type=int)
+    page_results = Result.query.order_by(Result.rating.desc()).paginate(page=page, per_page=5)
+    return render_template('rate_high.html', results=page_results, origin=city)
+
+
+@app.route('/rate_low', methods=['GET', 'POST'])
+def rate_low():
+    city = Result.query.with_entities(Result.origin).limit(1).scalar()
+    form = SearchForm()
+    page = request.args.get('page', 1, type=int)
+    page_results = Result.query.order_by(Result.rating.asc()).paginate(page=page, per_page=5)
+    return render_template('rate_low.html', form=form, results=page_results, origin=city)
+
+
+@app.route('/time_fast', methods=['GET', 'POST'])
+def time_fast():
+    city = Result.query.with_entities(Result.origin).limit(1).scalar()
+    form = SearchForm()
+    page = request.args.get('page', 1, type=int)
+    page_results = Result.query.order_by(Result.duration.asc()).paginate(page=page, per_page=5)
+    return render_template('time_fast.html', form=form, results=page_results, origin=city)
+
+
+@app.route('/time_slow', methods=['GET', 'POST'])
+def time_slow():
+    city = Result.query.with_entities(Result.origin).limit(1).scalar()
+    form = SearchForm()
+    page = request.args.get('page', 1, type=int)
+    page_results = Result.query.order_by(Result.duration.desc()).paginate(page=page, per_page=5)
+    return render_template('time_slow.html', form=form, results=page_results, origin=city)
