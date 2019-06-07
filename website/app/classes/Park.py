730cd747b0e62dc21726a559ea8400076b38976c
Taiga Ishida <taigaishida@Taigas-Mac-mini.local>
2019-06-07 12:27:11 -0700

    simple park object

--- website/app/classes/Park.py
+++ website/app/classes/Park.py
@@ -1,3 +1,17 @@
 class Park(object):
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        self.destination = None
+        self.distance = None
+        self.duration = None
+        '''Initialize the object from the data'''
+        self.__initialize__(data)
+
+    def __initialize__(self, data):
+        '''TODO'''
+        temp = list(data)
+        self.destination = temp[0]
+        self.distance = temp[1]['text']
+        self.duration = temp[2]['text']
+
+    def __repr__(self):
+        return f'Destination: {self.destination}\nDistance: {self.distance}\nETA: {self.duration}'
--- website/app/routes.py
+++ website/app/routes.py
@@ -8,7 +8,6 @@ from app.classes.Park import Park
 
 import time
 
-
 gmaps = googlemaps.Client(key=API_KEY)
 query = ['skatepark', 'skate park']
 default_url = 'https://maps.googleapis.com/maps/api/place/photo?'
@@ -27,10 +26,19 @@ def home():
     return render_template('home.html', form=form)
 
 
+def build_destination(destinations, distances, durations):
+    # [(destination, distance, duration)]
+    return list(zip(destinations, distances, durations))
+
+
+def make_parks(data):
+    return [Park(x) for x in data]
+
+
 @app.route('/results', methods=['GET', 'POST'])
 def results():
     start = time.time()
-    places_results=[]
+    places_results = []
     form = SearchForm()
     if form.validate_on_submit():
         geolocator = Nominatim(user_agent="myapplication")
@@ -39,58 +47,75 @@ def results():
         location = geolocator.geocode(city)
         longitude = location.longitude
         latitude = location.latitude
-        skatepark_result = gmaps.places(query=query[0] or query[1], radius=1000, location=f'{latitude}, {longitude}')['results']
+        skatepark_result = gmaps.places(
+            query=query[0] or query[1],
+            radius=1000,
+            location=f'{latitude}, {longitude}')['results']
         address_list = [park['formatted_address'] for park in skatepark_result]
         address_string = '|'.join(address_list)
-
         a = time.time()
-        desp = gmaps.distance_matrix(origins=f'{latitude}, {longitude}', destinations=address_string, transit_mode='driving')
-        print(desp)
+        desp = gmaps.distance_matrix(origins=f'{latitude}, {longitude}',
+                                     destinations=address_string,
+                                     transit_mode='driving')
+        destinations = desp['destination_addresses']
+        durations = [
+            element['duration'] for element in desp['rows'][0]['elements']
+        ]
+        distances = [
+            element['distance'] for element in desp['rows'][0]['elements']
+        ]
+        dest_info = build_destination(destinations, distances, durations)
+        parks = make_parks(dest_info)
+        for park in parks:
+            print(park)
         print(f'\n\n\n A: Elapsed = {time.time() - a}\n\n\n\n')
 
-        for park in skatepark_result:
-            b = time.time()
-            address = park['formatted_address']
-            distance_response = gmaps.distance_matrix(origins=f'{latitude}, {longitude}', destinations=address, transit_mode='driving')
-            distance = distance_response['rows'][0]['elements'][0]['distance']['text']
-            duration = distance_response['rows'][0]['elements'][0]['duration']['text']
+    #     for park in skatepark_result:
+    #         b = time.time()
+    #         address = park['formatted_address']
+    #         distance_response = gmaps.distance_matrix(origins=f'{latitude}, {longitude}', destinations=address, transit_mode='driving')
+    #         distance = distance_response['rows'][0]['elements'][0]['distance']['text']
+    #         duration = distance_response['rows'][0]['elements'][0]['duration']['text']
 
-            # retrieve photo url (some don't have photos)
-            try:
-                for photo in park['photos']:
-                    reference = photo['photo_reference']
-                    photo_url = requests.get(default_url + 'maxwidth=' + width +'&photoreference=' + reference + '&key=' + API_KEY).url
-            except Exception as e:
-                print(f'{e}')
+    #         # retrieve photo url (some don't have photos)
+    #         try:
+    #             for photo in park['photos']:
+    #                 reference = photo['photo_reference']
+    #                 photo_url = requests.get(default_url + 'maxwidth=' + width +'&photoreference=' + reference + '&key=' + API_KEY).url
+    #         except Exception as e:
+    #             print(f'{e}')
 
-            entry = Result(origin=city,
-                            name=park['name'],
-                            address=park['formatted_address'],
-                            rating=park['rating'],
-                            distance=distance,
-                            duration=duration,
-                            photo=photo_url)
-            places_results.append(entry)
-            print(city, park['name'], address, park['rating'], distance, duration, photo_url)
-            print(f'speed = {time.time() - b}')
-        db_reset()
-        db.session.add_all(places_results)
-        db.session.commit()
+    #         entry = Result(origin=city,
+    #                         name=park['name'],
+    #                         address=park['formatted_address'],
+    #                         rating=park['rating'],
+    #                         distance=distance,
+    #                         duration=duration,
+    #                         photo=photo_url)
+    #         places_results.append(entry)
+    #         print(city, park['name'], address, park['rating'], distance, duration, photo_url)
+    #         print(f'speed = {time.time() - b}')
+    #     db_reset()
+    #     db.session.add_all(places_results)
+    #     db.session.commit()
 
-    # pagination
-    page = request.args.get('page', 1, type=int)
-    page_results = Result.query.order_by(Result.duration.asc()).paginate(page=page, per_page=5)
+    # # pagination
+    # page = request.args.get('page', 1, type=int)
+    # page_results = Result.query.order_by(Result.duration.asc()).paginate(page=page, per_page=5)
 
-    print(f'Finished: Exec time = {time.time() - start}')
+    # print(f'Finished: Exec time = {time.time() - start}')
 
-    return render_template('results.html', form=form, results=page_results, origin=city)
+    # return render_template('results.html', form=form, results=page_results, origin=city)
+    return 'CONSTRUCTION'
+
 
 # sort by routes
 @app.route('/rate_high', methods=['GET', 'POST'])
 def rate_high():
     city = Result.query.with_entities(Result.origin).limit(1).scalar()
     page = request.args.get('page', 1, type=int)
-    page_results = Result.query.order_by(Result.rating.desc()).paginate(page=page, per_page=5)
+    page_results = Result.query.order_by(Result.rating.desc()).paginate(
+        page=page, per_page=5)
     return render_template('rate_high.html', results=page_results, origin=city)
 
 
@@ -99,8 +124,12 @@ def rate_low():
     city = Result.query.with_entities(Result.origin).limit(1).scalar()
     form = SearchForm()
     page = request.args.get('page', 1, type=int)
-    page_results = Result.query.order_by(Result.rating.asc()).paginate(page=page, per_page=5)
-    return render_template('rate_low.html', form=form, results=page_results, origin=city)
+    page_results = Result.query.order_by(Result.rating.asc()).paginate(
+        page=page, per_page=5)
+    return render_template('rate_low.html',
+                           form=form,
+                           results=page_results,
+                           origin=city)
 
 
 @app.route('/time_fast', methods=['GET', 'POST'])
@@ -108,8 +137,12 @@ def time_fast():
     city = Result.query.with_entities(Result.origin).limit(1).scalar()
     form = SearchForm()
     page = request.args.get('page', 1, type=int)
-    page_results = Result.query.order_by(Result.duration.asc()).paginate(page=page, per_page=5)
-    return render_template('time_fast.html', form=form, results=page_results, origin=city)
+    page_results = Result.query.order_by(Result.duration.asc()).paginate(
+        page=page, per_page=5)
+    return render_template('time_fast.html',
+                           form=form,
+                           results=page_results,
+                           origin=city)
 
 
 @app.route('/time_slow', methods=['GET', 'POST'])
@@ -117,5 +150,9 @@ def time_slow():
     city = Result.query.with_entities(Result.origin).limit(1).scalar()
     form = SearchForm()
     page = request.args.get('page', 1, type=int)
-    page_results = Result.query.order_by(Result.duration.desc()).paginate(page=page, per_page=5)
-    return render_template('time_slow.html', form=form, results=page_results, origin=city)
+    page_results = Result.query.order_by(Result.duration.desc()).paginate(
+        page=page, per_page=5)
+    return render_template('time_slow.html',
+                           form=form,
+                           results=page_results,
+                           origin=city)
