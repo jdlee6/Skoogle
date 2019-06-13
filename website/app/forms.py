3077e5507bb8393f1854b550004c1b23d94e5044
Joe Lee <jdlee6@buffalo.edu>
2019-06-13 17:59:04 -0400

    layout edit

--- website/app/forms.py
+++ website/app/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class SearchForm(FlaskForm):
-    location = StringField('Enter City or Zip Code Below', validators=[DataRequired(), Length(min=4)])
-    radius = StringField('Enter the amount of miles: ', validators=[DataRequired(), Length(min=1, max=2)])
+    location = StringField('Enter the city or zip code below:', validators=[DataRequired(), Length(min=4)])
+    radius = StringField('Enter the amount of miles below:', validators=[DataRequired(), Length(min=1, max=2)])
     submit = SubmitField('Search')
