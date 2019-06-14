edcdc3c73ca713c822cb3e66de63fb320bbf641b
Joe Lee <jdlee6@buffalo.edu>
2019-06-14 16:42:24 -0400

    finalized layout & postgresql on server

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -4,10 +4,10 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'AAAAA'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@35.226.56.130/postgres'
 db = SQLAlchemy(app)
 API_KEY = os.environ.get('API_KEY')
-print(API_KEY)
+# print(API_KEY)
 
 
 from app import routes
