d0b26ba6145e06b8eb152a2684069ddd674a6438
Joe Lee <jdlee6@buffalo.edu>
2019-07-15 21:42:10 -0400

    updated todo & psql

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'AAAAA'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@35.226.56.130/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@35.236.221.100/postgres'
 db = SQLAlchemy(app)
 API_KEY = os.environ.get('API_KEY')
 # print(API_KEY)
